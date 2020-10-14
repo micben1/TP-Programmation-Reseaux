a3e1e0dd2001a7a40c0d6d584e916419b4f075e9
micben1 <50212121+micben1@users.noreply.github.com>
2020-10-14 13:07:41 +0200

    Add files via upload

---
+++ ServerThread.java
@@ -0,0 +1,33 @@
+package stream;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ServerThread extends Thread {
+		
+	private Socket clientSocket;
+	private BufferedReader socIn;
+	
+	ServerThread(Socket s) {
+		this.clientSocket = s;
+	}
+
+ 	/**
+  	* receives a request from the clientThread then read the message
+  	* @param clientSocket
+  	**/
+	public void run() {
+    	  try {
+    		socIn = new BufferedReader(
+    			new InputStreamReader(clientSocket.getInputStream())); 
+    		while (true) {
+    		  String line = socIn.readLine();
+    		  System.out.println("message: "+line);
+    		}
+    	} catch (Exception e) {
+        	System.err.println("Error in clientSocket:" + e); 
+        }
+          //socIn.close();
+       }
+
+}
