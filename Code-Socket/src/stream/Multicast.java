2cff18697e73458b21246ab991dea40427288838
Pierre-Louis Jallerat <pierre-louis.jallerat@insa-lyon.fr>
2020-10-17 17:11:08 +0200

    pull http

--- Code-Socket/src/stream/Multicast.java
+++ Code-Socket/src/stream/Multicast.java
@@ -96,7 +96,7 @@ class Multicast {
    public static void main(String[] arg) throws Exception{ 
 		String nom = arg[0];
 		InetAddress groupeIP = InetAddress.getByName("239.255.80.84");
-		int port = 8084; 
+		int port = 1234; 
 		new Emetteur(groupeIP, port, nom);
 		new Recepteur(groupeIP, port, nom);
    }
