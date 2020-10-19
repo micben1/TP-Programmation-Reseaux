19d9de6fd0f1e4750185288fc9b3ffad02bebd07
Pierre-Louis Jallerat <pierre-louis.jallerat@insa-lyon.fr>
2020-10-19 11:39:52 +0200

    Lecture du body

--- HTTP/src/http/server/WebServer.java
+++ HTTP/src/http/server/WebServer.java
@@ -58,24 +58,32 @@ public class WebServer {
         // blank line signals the end of the client HTTP
         // headers.
         String str = ".";
-        String completeMessage = "";
-        boolean msgNull = false;
-        while (str != null && !str.equals("")) {
-        		str = in.readLine();
-        	if (str == null) {
-        		msgNull = true;
-        		break;
-        	} else {
-        	completeMessage  += str + "\n";
-        	}
-        }
-
-        if (!msgNull) {
-        	String[] headerInfo = new String[2];
-            readHeader(completeMessage, headerInfo);
+        String header = "";
+        while (str != null && !str.isEmpty()) {
+    		str = in.readLine();
+    		header  += str + "\n";
+      	}
+        
+        //Gère le premier msg nul
+        if (!header.substring(0, 4).equals("null") && header.length() > 5) {
+        	String[] headerInfo = new String[4];
+        	/*
+        	headerInfo:
+        	0: method (GET, PUT, etc.)
+        	1: URL
+        	2: body length
+        	3: body content
+        	*/
+            readHeader(header, headerInfo);
             String method = headerInfo[0];
             String url = headerInfo[1];
+            // int bodyLength = Integer.parseInt(headerInfo[2]);
             
+            for (int i = 0; i < 4; i++) {
+            	System.out.println(headerInfo[i]);
+            }
+            // String body = readBody(in, bodyLength);
+            // System.out.println(body);
             switch(method) {
             case "GET":
             	httpGET(bufOut, url);
@@ -92,14 +100,44 @@ public class WebServer {
         }
         
 		bufOut.close();
-
         remote.close();
       } catch (Exception e) {
+    	e.printStackTrace();
         System.out.println("Error: " + e);
       }
     }
   }
  
+  void readHeader (String completeMsg, String[] headerInfo) {
+	  if (completeMsg != null && completeMsg.length() > 0) {
+		  String arr[] = completeMsg.split("[ \n]");
+		  //method
+		  headerInfo[0] = arr[0];
+		  //url
+		  headerInfo[1] = arr[1].substring(1,arr[1].length());
+		  for (int u = 0; u<arr.length; u++) {
+			  if (arr[u].indexOf("Length") != -1) {
+				  headerInfo[2] = arr[u+1];
+			  }
+			  if (arr[u].indexOf("Type") != -1) {
+				  headerInfo[3] = arr[u+1];
+			  }
+		  }
+	  }
+  }
+  
+  String readBody (BufferedReader in, int length) {
+	  String body = "";
+	  try {
+		  char buff[] = new char[length];
+		  in.read(buff, 0, length);
+		  body = new String(buff);
+	  } catch(Exception e) {
+		  e.printStackTrace();
+	  }
+	  return body;
+  }
+  
 private void httpDELETE(BufferedOutputStream bufOut, String url) {
 	String header =  "";
 	  String arr[] = url.split("\\.");
@@ -121,6 +159,7 @@ private void httpDELETE(BufferedOutputStream bufOut, String url) {
 			  bufOut.flush(); 
 		  }  
 	  } catch(Exception e) {
+		  e.printStackTrace();
 		  try {
 			  header = makeHeader("500 Internal Server Error", null);
 			  bufOut.write(header.getBytes());
@@ -149,29 +188,6 @@ void writeFileInBufOut (File ressource, BufferedOutputStream bufOut, String url)
 	  }
   }
   
-  void readHeader (String completeMsg, String[] headerInfo) {
-	  if (completeMsg != null && completeMsg.length() > 0) {
-		  String arr[] = completeMsg.split(" ");
-		  //method
-		  headerInfo[0] = arr[0];
-		  //url
-		  headerInfo[1] = arr[1].substring(1,arr[1].length());
-		  //System.out.println("method: " + headerInfo[0]);
-		  //System.out.println("url: " +  headerInfo[1]);
-		  //System.out.println();
-		  //System.out.println(completeMsg);
-		  //System.out.println();
-		  //String[] a = completeMsg.split("\n\\s*\n");
-		  //System.out.println("header: " + a[0]);
-		  //System.out.println("body: " +  a[1]);
-	  }
-
-  }
-  
-  void readBody () {
-	  
-  }
-  
   public void httpGET(BufferedOutputStream bufOut, String url){
 	  String header =  "";
 	  String arr[] = url.split("\\.");
@@ -189,6 +205,7 @@ void writeFileInBufOut (File ressource, BufferedOutputStream bufOut, String url)
 			  bufOut.flush();
 		  }
 	  } catch(Exception e) {
+		  e.printStackTrace();
 		  try {
 			  header = makeHeader("500 Internal Server Error", null);
 			  bufOut.write(header.getBytes());
@@ -213,6 +230,7 @@ void writeFileInBufOut (File ressource, BufferedOutputStream bufOut, String url)
 			  bufOut.flush();
 		  }
 	  } catch(Exception e) {
+		  e.printStackTrace();
 		  try {
 			  header = makeHeader("500 Internal Server Error", null);
 			  bufOut.write(header.getBytes());
